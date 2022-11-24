27c626b768eab7898c2597ffffa52e1dbc2e3a53
zakrnem <zakrnem@zohomail.com>
2022-11-23 21:13:02 -0600

    Inital header layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
+import './style.css';
 import { newTask } from "./newTask.js";
-newTask()
+
+function homeDOM() {
+    let content = document.querySelector('#content')
+    let header = document.createElement('header')
+    
+    let sidebarButton = document.createElement('button')
+    sidebarButton.className = 'header-button'
+    sidebarButton.id = 'sidebar-button'
+    let sidebarIcon = document.createElement('img')
+    sidebarIcon.src = './img/sidebar-close-svgrepo-com.svg'
+    sidebarIcon.className = 'icon'
+    sidebarButton.appendChild(sidebarIcon)
+    header.appendChild(sidebarButton)
+
+    content.appendChild(header)
+}
+
+homeDOM()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      }
+    ]
+  }
 };
