993c153aae3756b379abddec917905191944c423
zakrnem <zakrnem@zohomail.com>
2022-11-26 10:37:57 -0600

    Add sidebar buttons

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ function homeDOM() {
     let content = document.querySelector('#content')
     let header = document.createElement('header')
     
-    let sideBarButton = newIcon('header-button', 'sidebar-button', 'sidebar-close-svgrepo-com.svg')
+    let sideBarButton = newIcon('header-button', 'sidebar-button',
+    'sidebar-close-svgrepo-com.svg', 'header-icon')
     header.appendChild(sideBarButton)
 
-    let homeButton = newIcon('header-button','home-button', 'home-svgrepo-com.svg')
+    let homeButton = newIcon('header-button','home-button',
+    'home-svgrepo-com.svg', 'header-icon')
     header.appendChild(homeButton)
 
     let searchBar = document.createElement('input')
@@ -17,10 +19,12 @@ function homeDOM() {
     searchBar.id = 'search-bar'
     header.appendChild(searchBar)
 
-    let searchButton = newIcon('header-button', 'search-button', 'search-svgrepo-com.svg')
+    let searchButton = newIcon('header-button', 'search-button',
+    'search-svgrepo-com.svg', 'header-icon')
     header.appendChild(searchButton)
 
-    let plusButton = newIcon('header-button','plus-button', 'plus-svgrepo-com.svg')
+    let plusButton = newIcon('header-button','plus-button',
+    'plus-svgrepo-com.svg', 'header-icon')
     header.appendChild(plusButton)
 
     content.appendChild(header)
@@ -28,13 +32,14 @@ function homeDOM() {
 
 homeDOM()
 
-function newIcon(className, id, imgSrc) {
+function newIcon(className, id, imgSrc, iconClass, buttonText) {
     let newButton = document.createElement('button')
     newButton.className = className
     newButton.id = id
+    newButton.textContent = buttonText
     let newIcon = document.createElement('img')
     newIcon.src = `./img/${imgSrc}`
-    newIcon.className = 'icon'
+    newIcon.className = iconClass
     newButton.appendChild(newIcon)
     
     return newButton
@@ -46,8 +51,18 @@ function sidebarDOM() {
     let sidebar = document.createElement('div')
     sidebar.className = 'sidebar'
 
+    let taskButton = newIcon('sidebar-button', 'add-task',
+    'plus-svgrepo-com.svg', 'sidebar-icon', 'New Task')
+    sidebar.appendChild(taskButton)
+
+    let projectsButton = newIcon('sidebar-button', 'project-dropdown',
+    'plus-svgrepo-com.svg', 'sidebar-icon', 'Projects')
+    sidebar.appendChild(projectsButton)
+    
     content.appendChild(sidebar)
 }
+
+sidebarDOM()
 
 function dashboardDOM() {
     let content = document.querySelector('#content')
