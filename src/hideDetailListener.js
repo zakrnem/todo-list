bcf6b3ab08be60d331cff81c04befedf9b7116e6
zakrnem <zakrnem@zohomail.com>
2022-12-07 19:56:38 -0600

    Flip detail button working partially

--- src/hideDetailListener.js
+++ src/hideDetailListener.js
@@ -1,10 +1,22 @@
 export function hideTaskDetailListener() {
     document.addEventListener('click', (e) => {
-        if(e.target.parentElement.id === 'task-detail') {
-            let taskDetail = e.target.parentElement
-            .parentElement.nextSibling
-
-            taskDetail.style.display = 'none'
-        }
+        hideDetail(e)
+        flipDetailButton(e)
     })
+}
+
+function hideDetail(e) {
+    if(e.target.parentElement.id === 'task-detail') {
+        let taskDetail = e.target.parentElement
+        .parentElement.nextSibling
+
+        taskDetail.style.display = 'none'
+    }
+}
+
+function flipDetailButton(e) {
+    if(e.target.parentElement.id === 'task-detail') {
+        let detailTarget = e.target.parentElement
+        detailTarget.style.cssText = 'transform: rotate(180deg);transition: transform 1s;'
+    }
 }
