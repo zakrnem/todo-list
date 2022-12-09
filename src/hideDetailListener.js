faa15be74f45e76a4b2a4c1ccc1e9b2f69dcd9bb
zakrnem <zakrnem@zohomail.com>
2022-12-08 20:39:01 -0600

    Toggle task detail working partially

--- src/hideDetailListener.js
+++ src/hideDetailListener.js
@@ -1,22 +1,35 @@
 export function hideTaskDetailListener() {
     document.addEventListener('click', (e) => {
-        hideDetail(e)
-        flipDetailButton(e)
+        let selectedButton = e.target.parentElement.id
+        let taskDetail = e.target.parentElement
+        .parentElement.nextSibling
+        let detailTarget = e.target.parentElement
+        
+        hideDetail(selectedButton, taskDetail, detailTarget)
+        flipButtonDown(selectedButton, detailTarget)
     })
 }
 
-function hideDetail(e) {
-    if(e.target.parentElement.id === 'task-detail') {
-        let taskDetail = e.target.parentElement
-        .parentElement.nextSibling
-
+function hideDetail(selectedButton, taskDetail, detailTarget) {
+    if(selectedButton === 'task-detail'
+    && !detailTarget.outerHTML.includes('rotate')) {
         taskDetail.style.display = 'none'
+    }
+    else if (selectedButton === 'task-detail'
+    && detailTarget.outerHTML.includes('rotate')) {
+        taskDetail.style.display = 'flex'
     }
 }
 
-function flipDetailButton(e) {
-    if(e.target.parentElement.id === 'task-detail') {
-        let detailTarget = e.target.parentElement
-        detailTarget.style.cssText = 'transform-box: fill-box; transform-origin: center;transform: rotate(180deg);transition: transform 0.6s;'
+function flipButtonDown(selectedButton, detailTarget) {
+    if(selectedButton === 'task-detail') {
+        detailTarget.style.cssText = 
+         'transform-box: fill-box; transform-origin: center;transform: rotate(0deg);transition: transform 0.7s;'
     }
+}
+
+function pseudocode() {
+    //We can add an 'i' variable that logs the two states
+    //If 'i === -1' then the task detail are shown
+    //If 'i === 1' then the details are hidden
 }
