bc5e7e15de08c72ec9d19d3ba951aa68558462bc
zakrnem <zakrnem@zohomail.com>
2023-02-09 19:48:25 -0600

    Fix taskDetail & delete f() using includes

--- src/removeTaskListener.js
+++ src/removeTaskListener.js
@@ -2,7 +2,7 @@ export function removeTaskListener() {
     document.addEventListener('click', (e) => {
         let dashboard = document.querySelector('.dashboard')
 
-        if (e.target.parentElement.id === 'task-delete') {
+        if (e.target.parentElement.id.includes('task-delete')) {
             let task = e.target.parentElement.parentElement.parentElement
             dashboard.removeChild(task)
         }
--- src/toggleTaskDetail.js
+++ src/toggleTaskDetail.js
@@ -9,13 +9,13 @@ export function toggleTaskDetail() {
 }
 
 function toggleDetail(selectedButton, taskDetail, detailTarget) {
-    if(selectedButton === 'task-detail'
+    if(selectedButton.includes('task-detail')
     && !detailTarget.outerHTML.includes('rotate')) {
         taskDetail.style.display = 'none'
         detailTarget.style.cssText = 
          'transform-box: fill-box; transform-origin: center;transform: rotate(0deg);transition: transform 0.7s;'
     }
-    else if (selectedButton === 'task-detail'
+    else if (selectedButton.includes('task-detail')
     && detailTarget.outerHTML.includes('rotate')) {
         taskDetail.style.display = 'flex'
         detailTarget.style.cssText = ''
