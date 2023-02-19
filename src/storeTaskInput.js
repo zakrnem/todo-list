01eb67ee8330fa1e57aeaf45aa5eccfb66da07c9
zakrnem <zakrnem@zohomail.com>
2023-02-19 14:59:46 -0600

    taskDate() in separate file and working

--- src/storeTaskInput.js
+++ src/storeTaskInput.js
@@ -1,3 +1,5 @@
+import { taskDate } from "./taskDate"
+
 function storeTaskInput(taskCount) {
     const storedTasks = []
     for (let i=taskCount; i>0; i--) {
@@ -43,27 +45,4 @@ export function storeTaskListener(taskCount) {
         checkInputs(e)
         dateInput(e)
     })
-}
-
-function taskDate(e) {
-    const fullDate = new Date();
-    let nowDate = fullDate.getDate()
-    //if (nowDate length is equal to zero add a zero before the current number)
-    let currentDate = `${fullDate.getFullYear()}-${fullDate.getMonth()+1}-${nowDate}`
-    console.log(currentDate)
-    
-
-    //Make the today & tomorrow buttons change the input type date value
-
-    switch (true) {
-        case (e.target.textContent === 'Today'):
-            let taskCount = parseInt(e.target.id.match(/\d+$/)[0]);
-            let taskDateDisplay = document.getElementById(`date${taskCount}`)
-            taskDateDisplay.value = '2023-02-15'
-            break;
-        case (e.target.textContent === 'Tomorrow'):
-            taskDate = {month: nowDate.month, date: nowDate.date + 1, year: nowDate.year}
-            console.log(e.target.id)
-            break;
-    }
 }
---
+++ src/taskDate.js
@@ -0,0 +1,36 @@
+export function taskDate(e) {
+    let taskCount = parseInt(e.target.id.match(/\d+$/)[0]);
+    let taskDateDisplay = document.getElementById(`date${taskCount}`) 
+    
+    const fullDate = new Date();
+
+    let nowDate = fullDate.getDate().toString()
+    if (nowDate.length === 1) {
+        nowDate = 0 + nowDate
+    }
+
+    let tomorrowDayDate = (parseInt(nowDate)+1).toString()
+    if (tomorrowDayDate.length === 1) {
+        tomorrowDayDate = 0 + tomorrowDayDate
+    }
+
+    let nowMonth = (fullDate.getMonth()+1).toString()
+    if (nowMonth.length === 1) {
+        nowMonth = 0 + nowMonth
+    }
+
+    let currentDate = `${fullDate.getFullYear()}-${nowMonth}-${nowDate}`
+    let tomorrowDate = `${fullDate.getFullYear()}-${nowMonth}-${tomorrowDayDate}`
+    
+
+    //Make the today & tomorrow buttons change the input type date value
+
+    switch (true) {
+        case (e.target.textContent === 'Today'):
+            taskDateDisplay.value = currentDate
+            break;
+        case (e.target.textContent === 'Tomorrow'):
+            taskDateDisplay.value = tomorrowDate
+            break;
+    }
+}
