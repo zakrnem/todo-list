00c337ea5375fdc9115c61c463202f69dd0dea64
zakrnem <zakrnem@zohomail.com>
2023-02-28 21:22:30 -0600

    Refactor projectListener, fix projectCount bug

--- src/projectListener.js
+++ src/projectListener.js
@@ -4,32 +4,31 @@ import { clearDOM } from "./clearDOM"
 import { newProject } from "./projectNew"
 
 export function projectListener() {
-    //Insert project dashboard
+    let projectCount = 2
     document.addEventListener('click', (e) => {
-        if (e.target.id != '' && (e.target.id.includes('project-button') ||
-            e.target.parentElement.id.includes('project-button'))) {
-            //Bug in the if statement after removing return button
+        if (e.target.id != '') {
+            //Insert project dashboard
+            if (e.target.id.includes('project-button') ||
+            e.target.parentElement.id.includes('project-button')) {
             clearDOM()
             projectDOM(1) //Not adding personal project
+            //It doesn't add it because it's added in the projectDOM() only once
+            }
+
+            //Add a new project
+            if (e.target.id.includes('new-project')) {
+                newProject(projectCount, `New project #${projectCount}`)
+                projectCount++
+            }
         }
     })
-
-    //Go to project edit (task creation)
+    
     document.addEventListener('dblclick', (e) => {
+        //Go to project edit (task creation)
         if (e.target.id.includes('projectN')) {
             let projectNumber = e.target.id.match(/\d+$/)[0]
             let pjTitle = document.querySelector(`#projectT${projectNumber}`).value
             projectEdit(pjTitle)
         }
-    })
-
-    //Add a new project
-    let projectCount = 2
-    document.addEventListener('click', (e) => {
-        if (e.target.id != '' && e.target.id.includes('new-project')) {
-            newProject(projectCount, `New project #${projectCount}`)
-        }
-        projectCount++
-    })
-
+    }) 
 }
