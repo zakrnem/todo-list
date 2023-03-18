bc33cf4cde0191f9b4c46d9caa51a1d7f8d2b83b
zakrnem <zakrnem@zohomail.com>
2023-03-18 15:15:30 -0600

    Write pending changes

--- src/projectStorage.js
+++ src/projectStorage.js
@@ -1,10 +1,11 @@
-export function projectStorage(typeOfOperation, projTitle, projTasks) {
+export function projectStorage(typeOfOperation, projTitle, projTasks, /*projID*/) {
     if (typeOfOperation === 'read') {
         return storedProjects
     }
     if (typeOfOperation === 'write') {
         let found
         for (let key in storedProjects) {
+            //(storedProjects[key].id === projID)
             if (storedProjects[key].title === projTitle) {
                 storedProjects[key] = projTasks
                 let projectCount = storedProjects.length
