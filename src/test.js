e619aa0060328a4b2db0004fc5a1217994346292
zakrnem <zakrnem@zohomail.com>
2023-03-22 20:28:18 -0600

    Set storedProjects object for upcoming tasks development

--- src/test.js
+++ src/test.js
@@ -1,82 +1,110 @@
 let storedProjects = [
-    {
-        "title": "Months",
-        "tasks": [
-            {
-                "title": "January",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
+    [
+        {
+            "title": "Fiat",
+            "tasks": [
+                {
+                    "project": "Fiat",
+                    "title": "500",
+                    "description": "",
+                    "date": {
+                        "month": 2,
+                        "date": 2,
+                        "year": 2021
+                    },
+                    "completed": false
                 },
-                "completed": false
-            },
-            {
-                "title": "February",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
+                {
+                    "project": "Fiat",
+                    "title": "Uno",
+                    "description": "",
+                    "date": {
+                        "month": 2,
+                        "date": 13,
+                        "year": 2019
+                    },
+                    "completed": false
                 },
-                "completed": false
-            }
-        ],
-        "id": "1"
-    },
-    {
-        "title": "Days",
-        "tasks": [
-            {
-                "title": "Monday",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
+                {
+                    "project": "Fiat",
+                    "title": "Punto",
+                    "description": "",
+                    "date": {
+                        "month": 1,
+                        "date": 4,
+                        "year": 2017
+                    },
+                    "completed": false
+                }
+            ],
+            "id": "1"
+        },
+        {
+            "title": "Suzuki",
+            "tasks": [
+                {
+                    "project": "Suzuki",
+                    "title": "Jimny",
+                    "description": "",
+                    "date": {
+                        "month": 3,
+                        "date": 22,
+                        "year": 2023
+                    },
+                    "completed": false
                 },
-                "completed": false
-            },
-            {
-                "title": "Friday",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
+                {
+                    "project": "Suzuki",
+                    "title": "Swift",
+                    "description": "",
+                    "date": {
+                        "month": 3,
+                        "date": 23,
+                        "year": 2023
+                    },
+                    "completed": false
                 },
-                "completed": false
-            }
-        ],
-        "id": "2"
-    },
-    {
-        "title": "PC Brands",
-        "tasks": [
-            {
-                "title": "Asus",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
+                {
+                    "project": "Suzuki",
+                    "title": "Ignis",
+                    "description": "",
+                    "date": {
+                        "month": 6,
+                        "date": 28,
+                        "year": 2023
+                    },
+                    "completed": false
+                }
+            ],
+            "id": "2"
+        },
+        {
+            "title": "Hyundai",
+            "tasks": [
+                {
+                    "project": "Hyundai",
+                    "title": "i30",
+                    "description": "",
+                    "date": {
+                        "month": 3,
+                        "date": 1,
+                        "year": 2023
+                    },
+                    "completed": false
                 },
-                "completed": false
-            },
-            {
-                "title": "Acer",
-                "description": "",
-                "date": {
-                    "month": null,
-                    "date": null,
-                    "year": null
-                },
-                "completed": false
-            }
-        ],
-        "id": "3"
-    }
+                {
+                    "project": "Hyundai",
+                    "title": "i10",
+                    "description": "",
+                    "date": {
+                        "month": 12,
+                        "date": 7,
+                        "year": 2022
+                    },
+                    "completed": false
+                }
+            ],
+            "id": "3"
+        }
+    ]
 ]
-
-console.log(storedProjects.flat(3))
