381c91e492a353fcccae098e13f5a91f9aa995a8
zakrnem <zakrnem@zohomail.com>
2023-03-23 21:03:12 -0600

    Insert upcoming task dashboard

--- src/upcomingTasks.js
+++ src/upcomingTasks.js
@@ -1,9 +1,40 @@
 import { projectStorage } from "./projectStorage";
+import { clearDOM } from "./clearDOM";
+import { newIcon } from "./newIcon";
+import { appendDOM } from "./appendDOM";
 
 export function upcomingTasks() {
     let storedProjects = projectStorage('read')
     let flattened = storedProjects.flatMap(item => item.tasks);
-    let unsorted = flattened
     flattened.sort((a, b) => (a.date > b.date) ? 1 : -1)
     console.log(flattened)
+
+    upcomingTasksDOM()
+}
+
+
+function upcomingTasksDOM() {
+    clearDOM()
+
+    let sidebar = document.querySelector('.sidebar')
+    let returnButton = newIcon('sidebar-button', 'returnB1',
+    'return-back-svgrepo-com.svg', 'sidebar-icon', 'Return')
+    sidebar.appendChild(returnButton)
+
+    let dashboard = document.createElement('div')
+    dashboard.className = 'upcoming-dash'
+
+    let title = document.createElement('p')
+    title.className = 'upcoming-title'
+    title.textContent = 'Upcoming tasks'
+    dashboard.appendChild(title)
+
+    appendDOM(dashboard)
+
+    /* document.addEventListener('click', (e) => {
+        if (e.target.id.includes('today') ||
+        e.target.id.includes('tomorrow')) {
+            taskDate(e)
+        }
+    }) */
 }
