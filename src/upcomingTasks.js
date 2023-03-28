754b55d744addcc2f3f92ae6dac4b7ca60fd0859
zakrnem <zakrnem@zohomail.com>
2023-03-27 19:59:22 -0600

    Display upcoming tasks f() test

--- src/upcomingTasks.js
+++ src/upcomingTasks.js
@@ -30,90 +30,32 @@ function upcomingTasksDOM() {
     dashboard.appendChild(title)
 
     appendDOM(dashboard)
-
-    /* document.addEventListener('click', (e) => {
-        if (e.target.id.includes('today') ||
-        e.target.id.includes('tomorrow')) {
-            taskDate(e)
-        }
-    }) */
+    displayUpcomingTask('Completed', 'Test', '2023-27-03')
 }
 
-function displayUpcomingTask() {
+function displayUpcomingTask(tskCompletion, tskTitle, tskDate) {
     let dashboard = document.querySelector('.upcoming-dash')
 
-    let taskEdit = document.createElement('div')
-    taskEdit.className = 'task-container'
-    dashboard.appendChild(taskEdit)
+    let taskContainer = document.createElement('div')
+    taskContainer.className = 'task-container'
+        let taskDate = document.createElement('div')
+        taskDate.className = 'task-date'
+        taskDate.textContent = 'Date: ' + tskDate
+        taskContainer.appendChild(taskDate)
 
-    let taskContainer1 = document.createElement('div')
-    taskContainer1.className = 'task-container1'
-    taskEdit.appendChild(taskContainer1)
-
-        let taskCheck = document.createElement('input')
-        taskCheck.setAttribute("type", "checkbox")
-        taskCheck.className = 'task-completed'
-        taskCheck.checked = tskCheck
-        taskContainer1.appendChild(taskCheck)
-
-        let taskTitle = document.createElement('input')
-        taskTitle.setAttribute("type", "text")
-        taskTitle.placeholder = 'Title your task'
+        let taskTitle = document.createElement('div')
         taskTitle.className = 'task-title'
-        taskTitle.value = tskTitle
-        taskContainer1.appendChild(taskTitle)
+        taskTitle.textContent = 'Title: ' + tskTitle
+        taskContainer.appendChild(taskTitle)
 
-        let taskDetail = newIcon('task-detail-button', `task-detail${taskCount}`,
-        'down-svgrepo-com.svg', 'task-icon')
-        taskContainer1.appendChild(taskDetail)
+        let taskCompletion = document.createElement('div')
+        taskCompletion.className = 'task-completion'
+        taskCompletion.textContent = 'Status: ' + tskCompletion
+        taskContainer.appendChild(taskCompletion)
 
-        let taskDelete = newIcon('task-delete-button', `task-delete${taskCount}`,
-        'delete-svgrepo-com.svg', 'task-icon')
-        taskContainer1.appendChild(taskDelete)
-
-    let taskContainer2 = document.createElement('div')
-    taskContainer2.className = 'task-container2'
-    taskEdit.appendChild(taskContainer2)
-
-        let taskDescription = document.createElement('textarea')
-        taskDescription.setAttribute("rows", "8")
-        taskDescription.placeholder = 'Add a description'
-        taskDescription.className = 'task-description'
-        taskDescription.value = tskDescrip
-        taskContainer2.appendChild(taskDescription)
-
-        let taskContainer3 = document.createElement('div')
-        taskContainer3.className = 'task-container3'
-        taskContainer2.appendChild(taskContainer3)
-
-            let taskContainer4 = document.createElement('div')
-            taskContainer4.className = 'task-container4'
-            taskContainer3.appendChild(taskContainer4)
-
-                    let today = document.createElement('button')
-                    today.id = `today${taskCount}`
-                    today.textContent = 'Today'
-                    taskContainer4.appendChild(today)
-
-                    let tomorrow = document.createElement('button')
-                    tomorrow.id = `tomorrow${taskCount}`
-                    tomorrow.textContent = 'Tomorrow'
-                    taskContainer4.appendChild(tomorrow)
-                    
-                    let dueDate = document.createElement('input')
-                    dueDate.setAttribute("type", "date")
-                    dueDate.className = 'task-date'
-                    dueDate.id = `date${taskCount}`
-                    dueDate.value = tskDate
-                    taskContainer4.appendChild(dueDate)
-
-            let taskSubmit = document.createElement('button')
-
-            taskSubmit.textContent = 'Submit'
-            taskSubmit.className = 'task-submit'
-            taskContainer3.appendChild(taskSubmit)
-
+    dashboard.appendChild(taskContainer)
     appendDOM(dashboard)
+
     /*
     We should just display tasks that are uncompleted
     The following properties should be displayed:
